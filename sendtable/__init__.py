9bedd4512d437c46d62de6586579e5b31ad68514
Jochem Loedeman <j.m.loedeman@gmail.com>
2023-03-01 16:03:04 +0100

    Replace blob trigger with timer trigger for sendtable

--- sendtable/__init__.py
+++ sendtable/__init__.py
@@ -1,8 +1,17 @@
+import logging
+from datetime import datetime, timezone
+
 import azure.functions as func
 
 from sendtable.monthly_pr_table import image_bytes_to_buffer, send_monthly_pr_table
 
 
-def main(tableBlob: func.InputStream) -> None:
-    table_buffer = image_bytes_to_buffer(tableBlob.read())
+def main(monthlyTimer: func.TimerRequest, tableIn: bytes) -> None:
+    utc_timestamp = datetime.utcnow().replace(tzinfo=timezone.utc).isoformat()
+
+    if monthlyTimer.past_due:
+        logging.info("The timer is past due!")
+
+    table_buffer = image_bytes_to_buffer(tableIn)
     send_monthly_pr_table(table_buffer)
+    logging.info("Python timer trigger function ran at %s", utc_timestamp)
